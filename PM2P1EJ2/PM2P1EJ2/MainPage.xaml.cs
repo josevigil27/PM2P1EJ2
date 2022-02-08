30554b6dec8edbabc9b1d6fa70d3f43c3448c5a9
Jose Vigil <jvigil504@gmail.com>
2022-02-08 17:10:02 -0600

    PM2P1EJ2

---
+++ PM2P1EJ2/PM2P1EJ2/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM2P1EJ2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btnpagemain_Clicked(object sender, EventArgs e)
+        {
+            var Contactos = new Models.Contactos()
+            {
+                Nombre = this.txtnombre.Text,
+                Apellido = this.txtapellido.Text,
+                Edad = Convert.ToInt32(this.txtedad.Text),
+                CorreoElectronico = this.txtcorreo.Text
+            };
+
+            var pagina = new PageMain();
+            pagina.BindingContext = Contactos;
+            await Navigation.PushAsync(pagina);
+        }
+    }
+}
---
+++ PM2P1EJ2/PM2P1EJ2/Models/Contactos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM2P1EJ2.Models
+{
+    public class Contactos
+    {
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public Int32 Edad { get; set; }
+        public String CorreoElectronico { get; set; }
+    }
+}
